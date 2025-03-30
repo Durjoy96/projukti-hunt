73efc52e191ca3559003e81ef8613d8f85d518a5
Durjoy96 <mddurjoy853@gmail.com>
2025-03-30 13:39:10 +0600

    Add checklist items to Launch Checklist component for submission validation

--- src/app/posts/new/components/LaunchChecklist.jsx
+++ src/app/posts/new/components/LaunchChecklist.jsx
@@ -1,3 +1,5 @@
+import { Circle, CircleCheck } from "lucide-react";
+
 export default function LaunchChecklist({ setSubmissionInfo, submissionInfo }) {
   return (
     <>
@@ -9,7 +11,56 @@ export default function LaunchChecklist({ setSubmissionInfo, submissionInfo }) {
           </p>
         </div>
         <div className="mt-12">
-            
+          <ul className="grid grid-cols-2 space-y-2 text-base font-normal text-base-content">
+            <li className="flex gap-1.5 items-center">
+              {submissionInfo.product_name ? (
+                <CircleCheck className="w-5 h-5 stroke-primary" />
+              ) : (
+                <Circle className="w-5 h-5 stroke-base-content-secondary" />
+              )}
+              Product Name
+            </li>
+            <li className="flex gap-1.5 items-center">
+              {submissionInfo.tagline ? (
+                <CircleCheck className="w-5 h-5 stroke-primary" />
+              ) : (
+                <Circle className="w-5 h-5 stroke-base-content-secondary" />
+              )}
+              Tagline
+            </li>
+            <li className="flex gap-1.5 items-center">
+              {submissionInfo.description ? (
+                <CircleCheck className="w-5 h-5 stroke-primary" />
+              ) : (
+                <Circle className="w-5 h-5 stroke-base-content-secondary" />
+              )}
+              Product Description
+            </li>
+            <li className="flex gap-1.5 items-center">
+              {submissionInfo.web_app_link.includes(".") ? (
+                <CircleCheck className="w-5 h-5 stroke-primary" />
+              ) : (
+                <Circle className="w-5 h-5 stroke-base-content-secondary" />
+              )}
+              Website or App Link
+            </li>
+            <li className="flex gap-1.5 items-center">
+              {submissionInfo.category ? (
+                <CircleCheck className="w-5 h-5 stroke-primary" />
+              ) : (
+                <Circle className="w-5 h-5 stroke-base-content-secondary" />
+              )}
+              Category
+            </li>
+            <li className="flex gap-1.5 items-center">
+              {submissionInfo.logo ? (
+                <CircleCheck className="w-5 h-5 stroke-primary" />
+              ) : (
+                <Circle className="w-5 h-5 stroke-base-content-secondary" />
+              )}
+              Logo
+            </li>
+          </ul>
         </div>
       </div>
     </>
