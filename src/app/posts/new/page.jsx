a6b190202bd81be6e38bb9d14c9a0c65d466347a
Durjoy96 <mddurjoy853@gmail.com>
2025-03-30 15:38:05 +0600

    Add icons to tab buttons in SubmitProduct component for improved UI

--- src/app/posts/new/page.jsx
+++ src/app/posts/new/page.jsx
@@ -6,6 +6,7 @@ import CommunityAndSocialLinks from "./components/CommunityAndSocialLinks";
 import AdditionalFeatures from "./components/AdditionalFeatures";
 import { Button } from "@/components/ui/button";
 import LaunchChecklist from "./components/LaunchChecklist";
+import { CircleCheck, Info, MessageCircle, Plus } from "lucide-react";
 
 export default function SubmitProduct() {
   const [activeTab, setActiveTab] = useState("basic-information");
@@ -32,39 +33,59 @@ export default function SubmitProduct() {
         {/* sidebar */}
         <div className="flex flex-col gap-2">
           <button
-            className={`text-left hover:bg-base-300 pl-4 py-3 rounded-lg text-base-content font-medium cursor-pointer ${
+            className={`text-left hover:bg-base-300 pl-4 py-3 rounded-lg text-base-content font-medium cursor-pointer flex items-center gap-2 ${
               activeTab === "basic-information" &&
-              "bg-primary/20 hover:bg-primary/20"
+              "bg-primary/20 hover:bg-primary/20 text-primary"
             }`}
             onClick={() => setActiveTab("basic-information")}
           >
+            <Info
+              className={`w-6 h-6 stroke-base-content-secondary ${
+                activeTab === "basic-information" && "stroke-primary"
+              }`}
+            />{" "}
             Basic Information
           </button>
           <button
-            className={`text-left hover:bg-base-300 pl-4 py-3 rounded-lg text-base-content font-medium cursor-pointer ${
+            className={`text-left hover:bg-base-300 pl-4 py-3 rounded-lg text-base-content font-medium cursor-pointer flex items-center gap-2 ${
               activeTab === "community-social-links" &&
-              "bg-primary/20 hover:bg-primary/20"
+              "bg-primary/20 hover:bg-primary/20 text-primary"
             }`}
             onClick={() => setActiveTab("community-social-links")}
           >
+            <MessageCircle
+              className={`w-6 h-6 stroke-base-content-secondary ${
+                activeTab === "community-social-links" && "stroke-primary"
+              }`}
+            />{" "}
             Community & Social Links
           </button>
           <button
-            className={`text-left hover:bg-base-300 pl-4 py-3 rounded-lg text-base-content font-medium cursor-pointer ${
+            className={`text-left hover:bg-base-300 pl-4 py-3 rounded-lg text-base-content font-medium cursor-pointer flex items-center gap-2 ${
               activeTab === "additional-features" &&
-              "bg-primary/20 hover:bg-primary/20"
+              "bg-primary/20 hover:bg-primary/20 text-primary"
             }`}
             onClick={() => setActiveTab("additional-features")}
           >
-            Additional Features
+            <Plus
+              className={`w-6 h-6 stroke-base-content-secondary ${
+                activeTab === "additional-features" && "stroke-primary"
+              }`}
+            />{" "}
+            Additional
           </button>
           <button
-            className={`text-left hover:bg-base-300 pl-4 py-3 rounded-lg text-base-content font-medium cursor-pointer ${
+            className={`text-left hover:bg-base-300 pl-4 py-3 rounded-lg text-base-content font-medium cursor-pointer flex items-center gap-2 ${
               activeTab === "launch-checklist" &&
-              "bg-primary/20 hover:bg-primary/20"
+              "bg-primary/20 hover:bg-primary/20 text-primary"
             }`}
             onClick={() => setActiveTab("launch-checklist")}
           >
+            <CircleCheck
+              className={`w-6 h-6 stroke-base-content-secondary ${
+                activeTab === "launch-checklist" && "stroke-primary"
+              }`}
+            />
             Launch Checklist
           </button>
         </div>
