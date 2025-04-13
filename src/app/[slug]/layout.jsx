dcdbe21881e1f4ea14e73e8232442f5f5499a914
Durjoy96 <mddurjoy853@gmail.com>
2025-04-14 01:05:27 +0600

    Add Profile and Products components with user data retrieval and UI enhancements

---
+++ src/app/[slug]/layout.jsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useAuth } from "@/components/AuthProvider";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+export default function Profile({ children }) {
+  const { user } = useAuth();
+  const { slug } = useParams();
+  const [dbUser, setDbUser] = useState([]);
+  const [activeBtn, setActiveBtn] = useState("Upvotes");
+
+  useEffect(() => {
+    axios.get(`/api/auth/users?username=${slug}`).then((res) => {
+      console.log(res.data);
+      setDbUser(() => res.data);
+    });
+  }, [slug]);
+
+  return (
+    <>
+      <section className="max-w-4xl mx-auto px-5 mt-12">
+        {dbUser && (
+          <div className="flex items-center flex-col gap-6 bg-base-200 p-6 rounded-lg">
+            <Image
+              src={dbUser.photo_url}
+              width={100}
+              height={100}
+              className="rounded-full ring-primary"
+            />
+            <div>
+              <h3 className="text-2xl font-semibold text-base-content">
+                {dbUser.name}{" "}
+                <span className="text-base-content-secondary">
+                  {user && user.uid === dbUser._id && "(You)"}
+                </span>
+              </h3>
+              <span className="text-base-content-secondary text-sm font-medium mt-2">
+                @{dbUser.username}
+              </span>
+            </div>
+          </div>
+        )}
+        <div className="mt-12 md:mt-20 flex gap-6">
+          <Link href={`/${slug.replace("%40", "@")}`}>
+            <Button
+              variant="outline"
+              onClick={() => setActiveBtn("Upvotes")}
+              className={`rounded-full hover:text-primary hover:bg-primary/10 hover:border-transparent ${
+                activeBtn === "Upvotes" &&
+                "text-primary bg-primary/10 border-transparent"
+              }`}
+            >
+              Upvotes
+            </Button>
+          </Link>
+          <Link href={`/${slug.replace("%40", "@")}/products`}>
+            <Button
+              variant="outline"
+              onClick={() => setActiveBtn("Products")}
+              className={`rounded-full hover:text-primary hover:bg-primary/10 hover:border-transparent ${
+                activeBtn === "Products" &&
+                "text-primary bg-primary/10 border-transparent"
+              }`}
+            >
+              Products
+            </Button>
+          </Link>
+        </div>
+        {children}
+      </section>
+    </>
+  );
+}
--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,14 +1,19 @@
 "use client";
 
+import { useAuth } from "@/components/AuthProvider";
 import ProductCard from "@/components/ProductCard";
+import { Button } from "@/components/ui/button";
 import axios from "axios";
+import Image from "next/image";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 export default function Profile() {
+  const { user } = useAuth();
   const { slug } = useParams();
   const [upvotes, setUpvotes] = useState([]);
   const [dbUser, setDbUser] = useState([]);
+  const [activeBtn, setActiveBtn] = useState("Upvotes");
 
   useEffect(() => {
     axios.get(`/api/auth/users?username=${slug}`).then((res) => {
@@ -24,14 +29,12 @@ export default function Profile() {
 
   return (
     <>
-      <section className="max-w-5xl mx-auto px-5 mt-12">
-        {dbUser && <p>{dbUser.username}</p>}
-        <div className="max-w-4xl mt-8 grid">
-          {upvotes.map((product) => (
-            <ProductCard product={product} key={product._id} />
-          ))}
-        </div>
-      </section>
+      {" "}
+      <div className="max-w-4xl mt-8 grid">
+        {upvotes.map((product) => (
+          <ProductCard product={product} key={product._id} />
+        ))}
+      </div>
     </>
   );
 }
