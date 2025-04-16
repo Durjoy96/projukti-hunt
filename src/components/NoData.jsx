6b72d2b2b66d5e5dc967a26787d370bd7ea638b6
Durjoy96 <mddurjoy853@gmail.com>
2025-04-16 12:39:26 +0600

    Update NoData component image dimensions for consistency

--- src/components/NoData.jsx
+++ src/components/NoData.jsx
@@ -5,7 +5,7 @@ import NoDataIcon from "../assets/icons/no-data.svg";
 export default function NoData() {
   return (
     <div className="flex flex-col items-center my-20 text-center">
-      <Image src={NoDataIcon} alt="No Data" className="w-32 h-32" />
+      <Image src={NoDataIcon} alt="No Data" className="w-28 h-28" />
       <h3 className="text-lg font-medium text-base-content-secondary mt-3">
         No data found
       </h3>
