70dc6b7c2e2d3eb416ef4ec9ed07d5fb12122af0
Durjoy96 <mddurjoy853@gmail.com>
2025-05-03 20:17:43 +0600

    Refactor Footer component: enhance layout and styling for better presentation

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      <span>© 2025 Product Hunt</span>
+    <div className="max-w-4xl flex items-center justify-between pb-4 bg-transparent text-base-content">
+      <span className="text-base font-normal">© 2025 Projukti Hunt</span>
+      <a
+        href="https://www.facebook.com/ProjuktiHunt"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-sm font-normal text-base-content-secondary hover:text-primary transition-all delay-200 cursor-pointer flex items-center gap-2"
+      >
+        facebook
+      </a>
     </div>
   );
 }
