aee7f87b6b5b72896ada438b5912e18931d7f22c
Durjoy96 <mddurjoy853@gmail.com>
2025-05-04 17:48:11 +0600

    Refactor Footer component: restructure layout to group social media links for improved organization

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -4,14 +4,24 @@ export default function Footer() {
   return (
     <div className="max-w-4xl flex items-center justify-between pb-4 bg-transparent text-base-content">
       <span className="text-base font-normal">© 2025 Projukti Hunt</span>
-      <a
-        href="https://www.facebook.com/ProjuktiHunt"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-sm font-normal text-base-content-secondary hover:text-primary transition-all delay-200 cursor-pointer flex items-center gap-2"
-      >
-        facebook
-      </a>
+      <div className="flex items-center gap-4">
+        <a
+          href="https://www.facebook.com/share/g/16ZfZwyhMq/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sm font-normal text-base-content-secondary hover:text-primary transition-all delay-200 cursor-pointer flex items-center gap-2"
+        >
+          community
+        </a>
+        <a
+          href="https://www.facebook.com/ProjuktiHunt"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sm font-normal text-base-content-secondary hover:text-primary transition-all delay-200 cursor-pointer flex items-center gap-2"
+        >
+          facebook
+        </a>
+      </div>
     </div>
   );
 }
